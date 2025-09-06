042da4de164b954609f3787b7c7f9ee00e7c56c8
shimmingyu <alsrb60@petpuls.net>
2025-09-06 22:58:46 +0900

    favicon 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,6 +80,20 @@ export const metadata: Metadata = {
   verification: {
     google: "kaXPdPOQij_7YNOLITMfhdj3H2DIH8R4ozGUMVnJMRw",
   },
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    other: [
+      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
